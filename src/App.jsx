ff0f49d8511ac99eb42b9e2c8ab17e0678db8cdf
Vignesh242003 <vickyvignesh24072003@gmail.com>
2024-11-03 03:39:53 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Adivce } from './Adivce'
+// import { Reg } from './Reg'
+
+export const App = () => {
+  return (
+    <div>  
+      <Adivce/>
+      {/* <Reg/> */}
+    </div>
+  )
+}
